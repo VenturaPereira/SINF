18c2d55e2030a66bf04df38a1f549e826b61eb96
Peteraya <up201505460@fe.up.pt>
2018-11-22 19:56:34 +0000

    more pages

--- 360dashboard/resources/views/pages/financial.blade.php
+++ 360dashboard/resources/views/pages/financial.blade.php
@@ -1,7 +1,137 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="jumbotron text-center">
-        <h1>Welcome to financial</h1>
+    <div class="text-center">
+
+
+        <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+
+
+        <div class="well">
+                <h2>Assets</h2>
+                <h3>Current Assets</h3>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Account</th>
+                            <th>Balance in <%= financial_year.stop %></th>
+                            <th>Balance in <%= previous_financial_year.stop %></th>
+                            <th>Difference</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <% current_assets.forEach(function (account) { %>
+                        <tr>
+                            <td><%= account.number %> <%= account.name %></td>
+                            <td><%= account.balance.toFixed(2) %></td>
+                            <td><%= account.previous_balance.toFixed(2) %></td>
+                            <td><%= account.difference.toFixed(2) %></td>
+                        </tr>
+                        <% }) %>
+                        <tr>
+                            <td></td>
+                            <td>&Sigma; = <%= total_current_assets.toFixed(2) %></td>
+                            <td>&Sigma; = <%= previous_total_current_assets.toFixed(2) %></td>
+                            <td><%= (total_current_assets - previous_total_current_assets).toFixed(2) %></td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <h3>Non-Current Assets</h3>
+                <p>There is no non-current assets for this financial year.</p>
+
+                <h3>Total Assets</h3>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Balance in <%= financial_year.stop %></th>
+                            <th>Balance in <%= previous_financial_year.stop %></th>
+                            <th>Difference</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><%= total_current_assets.toFixed(2) %></td>
+                            <td><%= previous_total_current_assets.toFixed(2) %></td>
+                            <td><%= (total_current_assets - previous_total_current_assets).toFixed(2) %></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
     </div>
 @endsection
+
+
+<script>
+window.onload = function () {
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	title:{
+		text: "Crude Oil Reserves vs Production, 2016"
+	},
+	axisY: {
+		title: "Thousands billed",
+		titleFontColor: "#4F81BC",
+		lineColor: "#4F81BC",
+		labelFontColor: "#4F81BC",
+		tickColor: "#4F81BC"
+	},
+	axisY2: {
+		title: "Thousand sold",
+		titleFontColor: "#C0504E",
+		lineColor: "#C0504E",
+		labelFontColor: "#C0504E",
+		tickColor: "#C0504E"
+	},
+	toolTip: {
+		shared: true
+	},
+	legend: {
+		cursor:"pointer",
+		itemclick: toggleDataSeries
+	},
+	data: [{
+		type: "column",
+		name: "Billed (thousand/month)",
+		legendText: "Billed",
+		showInLegend: true,
+		dataPoints:[
+			{ label: "Saudi", y: 266.21 },
+			{ label: "Venezuela", y: 302.25 },
+			{ label: "Iran", y: 157.20 },
+			{ label: "Iraq", y: 148.77 },
+			{ label: "Kuwait", y: 101.50 },
+			{ label: "UAE", y: 97.8 }
+		]
+	},
+	{
+		type: "column",
+		name: "Sales (thousand/month)",
+		legendText: "Sales",
+		axisYType: "secondary",
+		showInLegend: true,
+		dataPoints:[
+			{ label: "Saudi", y: 10.46 },
+			{ label: "Venezuela", y: 2.27 },
+			{ label: "Iran", y: 3.99 },
+			{ label: "Iraq", y: 4.45 },
+			{ label: "Kuwait", y: 2.92 },
+			{ label: "UAE", y: 3.1 }
+		]
+	}]
+});
+chart.render();
+
+function toggleDataSeries(e) {
+	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	}
+	else {
+		e.dataSeries.visible = true;
+	}
+	chart.render();
+}
+
+}
+</script>
--- 360dashboard/resources/views/pages/inventory.blade.php
+++ 360dashboard/resources/views/pages/inventory.blade.php
@@ -1,7 +1,107 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="jumbotron text-center">
-        <h1>Welcome to inventory</h1>
+    <div>
+
+      <div class="container-fluid">
+        <h3>Sales orders </h3>
+        <table class="company_table">
+    <tr>
+      <th>#</th>
+      <th>ID</th>
+      <th>Product</th>
+      <th>Quantity</th>
+      <th>Client ID</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>101010</td>
+      <td>Banana</td>
+      <td>3</td>
+      <td>1010</td>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>101010</td>
+      <td>Banana</td>
+      <td>3</td>
+      <td>1010</td>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>101010</td>
+      <td>Banana</td>
+      <td>3</td>
+      <td>1010</td>
+    </tr>
+  </table>
+      </div>
+
+      <div class="container-fluid">
+        <h3>Stock </h3>
+        <table class="company_table">
+    <tr>
+      <th>#</th>
+      <th>Product</th>
+      <th>Quantity</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>Banana</td>
+      <td>3</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Banana</td>
+      <td>3</td>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>Banana</td>
+      <td>3</td>
+    </tr>
+  </table>
+      </div>
+
+
+      <div id="chartContainer" style="height: 300px; width: 50%;"></div>
     </div>
 @endsection
+
+
+
+<script>
+window.onload = function () {
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	theme: "light2",
+	animationEnabled: true,
+
+
+	data: [{
+		type: "pie",
+		indexLabelFontSize: 18,
+		radius: 100,
+		indexLabel: "{label} - {y}",
+		yValueFormatString: "###0.0\"%\"",
+		click: explodePie,
+		dataPoints: [
+			{ y: 42, label: "Gas" },
+			{ y: 21, label: "Nuclear"},
+			{ y: 24.5, label: "Renewable" },
+			{ y: 9, label: "Coal" },
+			{ y: 3.1, label: "Other Fuels" }
+		]
+	}]
+});
+chart.render();
+
+function explodePie(e) {
+	for(var i = 0; i < e.dataSeries.dataPoints.length; i++) {
+		if(i !== e.dataPointIndex)
+			e.dataSeries.dataPoints[i].exploded = false;
+	}
+}
+
+}
+</script>

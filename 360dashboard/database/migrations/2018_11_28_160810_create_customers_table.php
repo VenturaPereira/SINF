06946f8708b3d3675933a348de781a872904ff95
VenturaPereira <venturasousapereira@hotmail.com>
2018-11-28 16:28:19 +0000

    db

---
+++ 360dashboard/database/migrations/2018_11_28_160810_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->string('CustomerID');
+            $table->integer('AccountID');
+            $table->integer('CustomerTaxID');
+            $table->string('CompanyName');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}

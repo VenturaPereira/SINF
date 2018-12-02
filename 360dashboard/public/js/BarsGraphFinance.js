839149138bd5d1a76ff75eef6a6f3bedd77b32da
Diogo-PP <diogopeixotopereira@gmail.com>
2018-12-02 16:15:03 +0000

    Financial view almost done

--- 360dashboard/public/js/BarsGraphFinance.js
+++ 360dashboard/public/js/BarsGraphFinance.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  var id = document.getElementsByClassName("BarGraph")[0].id;
+  var id = document.getElementsByClassName("BarGraphFinance")[0].id;
   var arr = id.split("-");
   var title = arr[1];
   var y_axis = arr[2];
@@ -54,18 +54,18 @@ $(document).ready(function(){
               		legendText: y_axis_le,
               		showInLegend: true,
               		dataPoints:[
-              			{ label: "January", y: 266.21 },
-              			{ label: "February", y: 302.25 },
-              			{ label: "March", y: 157.20 },
-              			{ label: "April", y: 148.77 },
-              			{ label: "May", y: 101.50 },
+                    { label: "January", y: 26.21 },
+              			{ label: "February", y: 30.25 },
+              			{ label: "March", y: 15.20 },
+              			{ label: "April", y: 14.77 },
+              			{ label: "May", y: 10.50 },
               			{ label: "June", y: 97.8 },
-                    { label: "July", y: 266.21 },
-              			{ label: "August", y: 302.25 },
-              			{ label: "September", y: 157.20 },
-              			{ label: "October", y: 148.77 },
-              			{ label: "November", y: 101.50 },
-              			{ label: "December", y: 97.8 }
+                    { label: "July", y: 26.21 },
+              			{ label: "August", y: 30.25 },
+              			{ label: "September", y: 15.20 },
+              			{ label: "October", y: 14.77 },
+              			{ label: "November", y: 10.50 },
+              			{ label: "December", y: 9.8 }
               		]
               	},
               	{
@@ -75,17 +75,17 @@ $(document).ready(function(){
               		axisYType: "secondary",
               		showInLegend: true,
               		dataPoints:[
-                    { label: "January", y: 10.46 },
+                    { label: "January", y: 1.46 },
               			{ label: "February", y: 2.27 },
               			{ label: "March", y: 3.99 },
               			{ label: "April", y: 4.45 },
               			{ label: "May", y: 2.92 },
               			{ label: "June", y: 3.1 },
-                    { label: "July", y: 15.6 },
+                    { label: "July", y: 1.6 },
               			{ label: "August", y: 1.2 },
               			{ label: "September", y: 2.5 },
-              			{ label: "October", y: 8.9 },
-              			{ label: "November", y: 8.8 },
+              			{ label: "October", y: 4.9 },
+              			{ label: "November", y: 10.8 },
               			{ label: "December", y: 5.5 }
               		]
               	}]
--- 360dashboard/resources/views/pages/financial.blade.php
+++ 360dashboard/resources/views/pages/financial.blade.php
@@ -2,137 +2,100 @@
 
 @section('content')
 <script type="text/javascript" src="{{ URL::asset('js/BarsGraph.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('js/BarsGraphFinance.js') }}"></script>
+<link href="{{ URL::asset('css/financial.css') }}" rel="stylesheet">
+
     <div class="text-center">
 
+        <div class="BarGraph" id="BarChartContainer-Income Vs Expenses (2017)-Thousands-Thousands-Income-Expenses-Income (thousand)-Expenses (thousand)-postajax" style="height: 370px; width: 100%;"></div>
 
-        <div class="BarGraph" id="BarChartContainer-Financial Analysis-Thousands billed-Thousands sold-Billed-Sales-Billed (thousand/month)-Sold (thousand/month)-postajax" style="height: 370px; width: 100%;"></div>
-
-
-        <div class="well">
-                <h2>Assets</h2>
-                <h3>Current Assets</h3>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Account</th>
-                            <th>Balance in <%= financial_year.stop %></th>
-                            <th>Balance in <%= previous_financial_year.stop %></th>
-                            <th>Difference</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <% current_assets.forEach(function (account) { %>
-                        <tr>
-                            <td><%= account.number %> <%= account.name %></td>
-                            <td><%= account.balance.toFixed(2) %></td>
-                            <td><%= account.previous_balance.toFixed(2) %></td>
-                            <td><%= account.difference.toFixed(2) %></td>
-                        </tr>
-                        <% }) %>
-                        <tr>
-                            <td></td>
-                            <td>&Sigma; = <%= total_current_assets.toFixed(2) %></td>
-                            <td>&Sigma; = <%= previous_total_current_assets.toFixed(2) %></td>
-                            <td><%= (total_current_assets - previous_total_current_assets).toFixed(2) %></td>
-                        </tr>
-                    </tbody>
-                </table>
-
-                <h3>Non-Current Assets</h3>
-                <p>There is no non-current assets for this financial year.</p>
-
-                <h3>Total Assets</h3>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Balance in <%= financial_year.stop %></th>
-                            <th>Balance in <%= previous_financial_year.stop %></th>
-                            <th>Difference</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><%= total_current_assets.toFixed(2) %></td>
-                            <td><%= previous_total_current_assets.toFixed(2) %></td>
-                            <td><%= (total_current_assets - previous_total_current_assets).toFixed(2) %></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+        <div class="BarGraphFinance" id="BarChartFinanceContainer-Accounts receivable Vs Accounts payable (2017)-Thousands-Thousands-Billed-Sales-Billed (thousand)-Sold (thousand)-postajax" style="height: 370px; width: 100%;"></div>
     </div>
+
+    <div class="container-fluid" id="debtDiv">
+      <h3>Debt-To-Equity </h3>
+      <table class="company_table" id="debtTable">
+        <tr>
+            <th>Debt</th>
+            <th>Equity</th>
+            <th>Debt to Equity</th>
+        </tr>
+        <tr>
+             <td colspan=1>10 milhões</td>
+             <td colspan=1>30 milhões</td>
+             <td colspan=1>33,3%</td>
+        </tr>
+      </table>
+    </div>
+
+    <div class="container-fluid" id="invDiv">
+
+      <h3>Top Products </h3>
+      <table class="company_table" id="invTable">
+        <tr>
+          <th>Cost of goods sold</th>
+          <th>Average Inventory</th>
+          <th>Inventory turnover</th>
+        </tr>
+        <tr>
+             <td colspan=1>40 milhões</td>
+             <td colspan=1>15 milhões</td>
+             <td colspan=1>2.67</td>
+        </tr>
+      </table>
+    </div>
+
+
 @endsection
 
+<!--<div class="well">
+        <h2>Assets</h2>
+        <h3>Current Assets</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Account</th>
+                    <th>Balance in <%= financial_year.stop %></th>
+                    <th>Balance in <%= previous_financial_year.stop %></th>
+                    <th>Difference</th>
+                </tr>
+            </thead>
+            <tbody>
+                <% current_assets.forEach(function (account) { %>
+                <tr>
+                    <td><%= account.number %> <%= account.name %></td>
+                    <td><%= account.balance.toFixed(2) %></td>
+                    <td><%= account.previous_balance.toFixed(2) %></td>
+                    <td><%= account.difference.toFixed(2) %></td>
+                </tr>
+                <% }) %>
+                <tr>
+                    <td></td>
+                    <td>&Sigma; = <%= total_current_assets.toFixed(2) %></td>
+                    <td>&Sigma; = <%= previous_total_current_assets.toFixed(2) %></td>
+                    <td><%= (total_current_assets - previous_total_current_assets).toFixed(2) %></td>
+                </tr>
+            </tbody>
+        </table>
 
-<script>
-window.onload = function () {
+        <h3>Non-Current Assets</h3>
+        <p>There is no non-current assets for this financial year.</p>
 
-var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	title:{
-		text: "Crude Oil Reserves vs Production, 2016"
-	},
-	axisY: {
-		title: "Thousands billed",
-		titleFontColor: "#4F81BC",
-		lineColor: "#4F81BC",
-		labelFontColor: "#4F81BC",
-		tickColor: "#4F81BC"
-	},
-	axisY2: {
-		title: "Thousand sold",
-		titleFontColor: "#C0504E",
-		lineColor: "#C0504E",
-		labelFontColor: "#C0504E",
-		tickColor: "#C0504E"
-	},
-	toolTip: {
-		shared: true
-	},
-	legend: {
-		cursor:"pointer",
-		itemclick: toggleDataSeries
-	},
-	data: [{
-		type: "column",
-		name: "Billed (thousand/month)",
-		legendText: "Billed",
-		showInLegend: true,
-		dataPoints:[
-			{ label: "Saudi", y: 266.21 },
-			{ label: "Venezuela", y: 302.25 },
-			{ label: "Iran", y: 157.20 },
-			{ label: "Iraq", y: 148.77 },
-			{ label: "Kuwait", y: 101.50 },
-			{ label: "UAE", y: 97.8 }
-		]
-	},
-	{
-		type: "column",
-		name: "Sales (thousand/month)",
-		legendText: "Sales",
-		axisYType: "secondary",
-		showInLegend: true,
-		dataPoints:[
-			{ label: "Saudi", y: 10.46 },
-			{ label: "Venezuela", y: 2.27 },
-			{ label: "Iran", y: 3.99 },
-			{ label: "Iraq", y: 4.45 },
-			{ label: "Kuwait", y: 2.92 },
-			{ label: "UAE", y: 3.1 }
-		]
-	}]
-});
-chart.render();
-
-function toggleDataSeries(e) {
-	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-		e.dataSeries.visible = false;
-	}
-	else {
-		e.dataSeries.visible = true;
-	}
-	chart.render();
-}
-
-}
-</script>
+        <h3>Total Assets</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Balance in <%= financial_year.stop %></th>
+                    <th>Balance in <%= previous_financial_year.stop %></th>
+                    <th>Difference</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><%= total_current_assets.toFixed(2) %></td>
+                    <td><%= previous_total_current_assets.toFixed(2) %></td>
+                    <td><%= (total_current_assets - previous_total_current_assets).toFixed(2) %></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>-->

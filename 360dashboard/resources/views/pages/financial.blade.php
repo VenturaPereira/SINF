3d18df4641c2a8748a0e435551486184c08b686e
Diogo-PP <diogopeixotopereira@gmail.com>
2018-12-19 19:38:13 +0000

    financial backend done

--- 360dashboard/resources/views/pages/financial.blade.php
+++ 360dashboard/resources/views/pages/financial.blade.php
@@ -13,32 +13,28 @@
     </div>
 
     <div class="container-fluid" id="debtDiv">
-      <h3>Cash Related </h3>
-      <table class="company_table" id="CashTable">
-        <tr>
-            <th>Cash</th>
-            <th>Cash Ratio</th>
-        </tr>
-        <tr>
-             <td colspan=1>{{$cash}}</td>
-             <td colspan=1>{{$cash/$total_liabilities}}</td>
-        </tr>
-      </table>
+      <h3>Cash</h3>
+      <h5>{{round($cash,1)}}</h5>
     </div>
 
     <div class="container-fluid" id="debtDiv">
-      <h3>Current Ratio </h3>
-      <table class="company_table" id="debtTable">
-        <tr>
-            <th>Total Assets</th>
-            <th>Total Liabilities</th>
-            <th>Current Ratio</th>
-        </tr>
-        <tr>
-             <td colspan=1>{{$total_assets}}</td>
-             <td colspan=1>{{$total_liabilities}}</td>
-             <td colspan=1>{{$total_assets/$total_liabilities}}</td>
-        </tr>
-      </table>
+      <h3>Cash Ratio</h3>
+      <h5>{{round( ($cash/$total_liabilities), 2)}}</h5>
     </div>
+
+    <div class="container-fluid" id="debtDiv">
+      <h3>Total Assets</h3>
+      <h5>{{round($total_assets,1)}}</h5>
+    </div>
+
+    <div class="container-fluid" id="debtDiv">
+      <h3>Total Liabilities</h3>
+      <h5>{{round($total_liabilities, 1)}}</h5>
+    </div>
+
+    <div class="container-fluid" id="debtDiv">
+      <h3>Current Ratio</h3>
+      <h5>{{round( ($total_assets/$total_liabilities), 2)}}</h5>
+    </div>
+    
 @endsection

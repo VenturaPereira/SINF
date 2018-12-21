8dc2c76e7b53c0ae663446723e35f7d9dc14498e
bernardoleite <up201404464@gcloud.fe.up.pt>
2018-12-21 14:57:24 +0000

    Refractor

--- 360dashboard/resources/views/pages/saftmanager.blade.php
+++ 360dashboard/resources/views/pages/saftmanager.blade.php
@@ -5,7 +5,7 @@
 
     <form action = "{{route('file.store') }}" method="post" enctype="multipart/form-data">
         {{ csrf_field() }}
-        <h3> Upload SAFT and check there is connection with VM (API) on port 4001 </h3>
+        <h3> Upload SAFT</h3>
         <div class="input-group mb-3">
             <div class="custom-file">
               <input required type="file" class="custom-file-input" name="file" id="file">
